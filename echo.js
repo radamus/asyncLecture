102af8594a675d5d19fb0f03e6bc41b881059eb3
Radek Adamus <adamus.r@gmail.com>
2013-11-24 22:50:11 +0100

    exceptions in callback and promise

--- echo.js
+++ echo.js
@@ -2,14 +2,26 @@ var when = require('when');
 function log(val) {console.log(val);}
 
 
-function echo(val, callback) {  setTimeout(function() {callback(val);}, Math.random()*1000);}
+function echo(val, callback) { if(val === null) {callback('null'); return; } setTimeout(function() {callback(null, val);}, Math.random()*1000);}
 
 
-echo(1, function(result){
+echo(1, function(err, result){
+	if(err) {
+		log(result); 
+		return;
+	}
 	log(result); 
-	echo(2, function(result){
+	echo(2, function(err, result){
+		if(err) {
+			log(result); 
+			return;
+		}
 		log(result);
-		echo(3, function(result){
+		echo(3, function(err, result){
+			if(err) {
+				log(result); 
+				return;
+			}
 			log(result);
 		});
 	});
@@ -20,15 +32,16 @@ echo(1, function(result){
 function defferedEcho(val) {
 	var deffered = when.defer();
 
-	echo(val, function(res) {
+	echo(val, function(err, res) {
+		if(err) { deffered.reject(err); return; }
 		deffered.resolve(res);
 	});
 
 	return deffered.promise;
 }
 
-defferedEcho(1)
+defferedEcho(null)
 .then(function(res) { log('deffered ' + res); return defferedEcho(2);})
 .then(function(res){log('deffered ' + res); return defferedEcho(3);} )
-.then(function(res){log('deffered ' + res); } );
+.then(function(res){log('deffered ' + res); }, function(err){log('deffered ' + err);} );
 
