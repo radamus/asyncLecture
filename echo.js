4de13e749a9efb91e281e9c66f8f19346cd72917
Radek Adamus <adamus.r@gmail.com>
2013-11-24 13:34:55 +0100

    callback refactorization

--- echo.js
+++ echo.js
@@ -3,6 +3,8 @@ function log(val) {console.log(val);}
 
 function echo(val, callback) {  setTimeout(function() {callback(val);}, 500);}
 
-echo(1, function(result){log(result);});
-echo(2, function(result){log(result);});
-echo(3, function(result){log(result);});
+function cb(result){log(result);}
+
+echo(1, cb);
+echo(2, cb);
+echo(3, cb);
