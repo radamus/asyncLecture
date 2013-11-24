28a7f3cab8e5c3fa629281fd7c6da83f5e5a1d0f
Radek Adamus <adamus.r@gmail.com>
2013-11-24 17:00:38 +0100

    defferedEcho and promise

--- echo.js
+++ echo.js
@@ -1,9 +1,9 @@
+var when = require('when');
 function log(val) {console.log(val);}
 
 
 function echo(val, callback) {  setTimeout(function() {callback(val);}, Math.random()*1000);}
 
-function cb(result){log(result);}
 
 echo(1, function(result){
 	log(result); 
@@ -14,3 +14,21 @@ echo(1, function(result){
 		});
 	});
 });
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
+function defferedEcho(val) {
+	var deffered = when.defer();
+
+	echo(val, function(res) {
+		deffered.resolve(res);
+	});
+
+	return deffered.promise;
+}
+
+defferedEcho(1)
+.then(function(res) { log('deffered ' + res); return defferedEcho(2);})
+.then(function(res){log('deffered ' + res); return defferedEcho(3);} )
+.then(function(res){log('deffered ' + res); } );
+
